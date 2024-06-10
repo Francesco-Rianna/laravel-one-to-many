fe81dfad7d3685dcf836e1aa6618b628790ba153
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-10 14:08:31 +0200

    done seed in types table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\ProjectsTableSeedes;
+use Database\Seeders\TypesTableSeedes;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,7 +23,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            ProjectsTableSeeder::class
+            ProjectsTableSeeder::class,
+            TypeTableSeeder::class
         ]);
     }
 }
